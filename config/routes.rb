7c067fb44b5fbb641ef80a187a0f6f6a25482644
yoshimisatou <youhes@gmail.com>
2016-06-11 09:42:42 +0000

    followings-followers homework-1

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+  get 'users/:id/followings', to: 'users#followings'
+  get 'users/:id/followers', to: 'users#followers'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
